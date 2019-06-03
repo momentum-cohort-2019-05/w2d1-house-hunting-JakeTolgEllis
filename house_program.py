53a937c4847d6915d24fec52393bb01fd9b9128c
JakeTolgEllis <jakevinylellis@gmail.com>
2019-06-03 15:29:27 -0400

    pyprac

---
+++ house_program.py
@@ -0,0 +1,15 @@
+annual_salary= int(input( "What is your annual salary?"))
+portion_saved= float(input("How much do you save each month?"))
+total_cost= int(input ("What is the cost of your dream home?"))
+r= 0.04
+current_savings= 0.00
+portion_down_payment= 0.25
+num_months= 0
+
+while current_savings < portion_down_payment:
+    
+
+
+
+
+
