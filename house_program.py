a2c47a87e9eac2a722db2de72f742638ebe261ca
JakeTolgEllis <jakevinylellis@gmail.com>
2019-06-06 12:28:21 -0400

    fixedsyntaxreturning1monthonly

--- house_program.py
+++ house_program.py
@@ -4,13 +4,14 @@ total_cost= int(input ("What is the cost of your dream home?"))
 r= 0.04
 current_savings= 0.00
 portion_down_payment= 0.25
+down_payment_total= (portion_down_payment * total_cost)
 num_months= 0
 
-while current_savings < portion_down_payment:
-    current_savings = (current_savings +(current_savings * (r/12))) + ((annual_salary/12)*portion_saved)
-    num_months= num_months+1
-    if current_savings >= portion_down_payment:
-       print("it will take you" + str(num_months) + "months to save up for your house")
+while current_savings < down_payment_total:
+    num_months = num_months + 1
+    current_savings = ((current_savings +(current_savings * (r/12))) + ((annual_salary/12)*portion_saved))
+    if current_savings >= down_payment_total:
+       print("it will take you " + str(num_months) + " months to save up for your house")
 
 
 
